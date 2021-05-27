6e935722b38d0ea6403ad55c6052fa7c7533d14c
Rishabh Rathore <beinglikerishabh@gmail.com>
2021-05-27 10:12:53 +0530

    5 basic sorts and binary search commit

---
+++ Sorting/Selection.py
@@ -0,0 +1,17 @@
+import sys
+A = [64, 25, 12, 22, 11]
+for i in range(len(A)):	
+	# Find the minimum element in remaining
+	# unsorted array
+	min_index = i
+	for j in range(i+1, len(A)):
+		if A[min_index] > A[j]:
+			min_index = j
+			
+	# Swap the found minimum element with
+	# the first element		
+	A[i], A[min_index] = A[min_index], A[i]
+
+print ("Sorted array")
+for i in range(len(A)):
+	print(A[i]),
